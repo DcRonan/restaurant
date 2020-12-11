9f91894cf3385d6a4f5cc0ba0a4590fd55fdd815
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-11 14:15:50 +0000

    format code

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-
 module.exports = {
   entry: {
     index: './assets/src/js/index.js',
